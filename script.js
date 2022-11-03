59bc42e586d415c7c394015d9ad2c917586d6b76
MatGon's Websites Development <109096019+mwd-oficial@users.noreply.github.com>
2022-11-03 18:30:18 -0300

    atualizei

--- script.js
+++ script.js
@@ -1,93 +1,74 @@
 var teste = true
 function bio1() {
     var bio = document.getElementById("bio")
-    var parbio = document.getElementById("parbio")
-    var parbio1 = document.getElementById("parbio1")
-    var parbio2 = document.getElementById("parbio2")
     var titbio = document.getElementById("titbio")
 
     if (teste == true) {
         if (window.matchMedia("(max-width: 600px)").matches) {
-            bio.style.height = "180vh"
-
-        } else if (window.matchMedia("(max-width: 850px)").matches) {
-            bio.style.height = "140vh"
+            titbio.style.padding = "10px"
+            titbio.style.fontSize = "14px"
+            titbio.style.width = "150px"
         } else {
-            bio.style.height = "100vh"
+            titbio.style.padding = "20px"
+            titbio.style.fontSize = "20px"
+            titbio.style.width = "280px"
         }
-        bio.style.maxWidth = "80vw"
-        bio.style.display = "block"
-        bio.style.pointerEvents = "none"
+        bio.style.maxHeight = "300vh"
+        bio.style.opacity = 1
+        bio.style.marginBottom = "50px"
+        bio.style.padding = "30px"
+        titbio.style.borderBottom = "0px"
+        titbio.style.borderRadius = "20px 20px 0px 0px"
         teste = false
-
-        setTimeout(() => {
-            parbio.style.display = "block"
-            parbio1.style.display = "block"
-            parbio2.style.display = "block"
-            bio.style.pointerEvents = "auto"
-        }, "2000")
-        setTimeout(() => {
-            titbio.style.fontSize = "25px"
-            bio.style.padding = "20px 0px"
-        }, "1500")
-
     } else {
-        bio.style.maxWidth = "100px"
-        bio.style.height = "50px"
-        bio.style.display = "flex"
-        bio.style.padding = "0px"
-        parbio.style.display = "none"
-        parbio1.style.display = "none"
-        parbio2.style.display = "none"
+        bio.style.maxHeight = 0
+        bio.style.opacity = 0
+        bio.style.padding = 0
+        titbio.style.padding = "10px"
         titbio.style.fontSize = "14px"
+        titbio.style.width = "150px"
+        bio.style.marginBottom = 0
+        titbio.style.borderBottom = "5px solid white"
+        titbio.style.borderRadius = "20px"
         teste = true
     }
-
+    
 }
 
 function pais1() {
     var pais = document.getElementById("pais")
-    var parpais = document.getElementById("parpais")
     var imgpais = document.getElementById("imgpais")
     var titpais = document.getElementById("titpais")
-
+    
     if (teste == true) {
         if (window.matchMedia("(max-width: 600px)").matches) {
-            pais.style.height = "50vh"
-
-        } else if (window.matchMedia("(max-width: 900px)").matches) {
-            pais.style.height = "70vh"
-
-        } else if (window.matchMedia("(max-width: 1200px)").matches) {
-            pais.style.height = "80vh"
-
+            titpais.style.padding = "10px"
+            titpais.style.fontSize = "14px"
+            titpais.style.width = "150px"
         } else {
-            pais.style.height = "90vh"
+            titpais.style.padding = "20px"
+            titpais.style.fontSize = "20px"
+            titpais.style.width = "280px"
+
         }
-        pais.style.maxWidth = "80vw"
-        pais.style.display = "block"
-        imgpais.style.marginTop = "25px"
-        pais.style.pointerEvents = "none"
+        pais.style.maxHeight = "300vh"
+        pais.style.opacity = 1
+        pais.style.padding = "30px"
+        pais.style.marginBottom = "50px"
+        titpais.style.borderBottom = "0px"
+        titpais.style.borderRadius = "20px 20px 0px 0px"
         teste = false
-
-        setTimeout(() => {
-            imgpais.style.display = "block"
-            parpais.style.display = "block"
-            pais.style.pointerEvents = "auto"
-        }, "2000")
-        setTimeout(() => {
-            titpais.style.fontSize = "25px"
-            pais.style.padding = "20px 0px"
-        }, "1500")
-
+        
     } else {
-        pais.style.maxWidth = "100px"
-        pais.style.height = "50px"
-        pais.style.display = "flex"
-        pais.style.padding = "0px"
-        imgpais.style.display = "none"
-        parpais.style.display = "none"
+        pais.style.maxHeight = 0
+        pais.style.opacity = 0
+        pais.style.padding = 0
+        titpais.style.padding = "10px"
         titpais.style.fontSize = "14px"
+        titpais.style.width = "150px"
+        pais.style.marginBottom = 0
+        titpais.style.borderBottom = "5px solid white"
+        titpais.style.borderRadius = "20px"
         teste = true
     }
 }
@@ -97,68 +78,74 @@ function disc1() {
     var titdisc = document.getElementById("titdisc")
 
     if (teste == true) {
-        disc.style.maxWidth = "80vw"
-        disc.style.height = "60vh"
-        disc.style.display = "block"
-        pardisc.style.marginTop = "50px"
-        disc.style.pointerEvents = "none"
+        if (window.matchMedia("(max-width: 600px)").matches) {
+            titdisc.style.padding = "10px"
+            titdisc.style.fontSize = "14px"
+            titdisc.style.width = "150px"
+            pardisc.style.maxWidth = "80vw"
+        } else {
+            titdisc.style.padding = "20px"
+            titdisc.style.fontSize = "20px"
+            titdisc.style.width = "280px"
+            pardisc.style.maxWidth = "67.5vw"
+
+        }
+        disc.style.maxHeight = "300vh"
+        disc.style.opacity = 1
+        disc.style.padding = "30px"
+        disc.style.marginBottom = "50px"
+        titdisc.style.borderBottom = "0px"
+        titdisc.style.borderRadius = "20px 20px 0px 0px"
         teste = false
-
-        setTimeout(() => {
-            pardisc.style.display = "block"
-            disc.style.pointerEvents = "auto"
-        }, "2000")
-        setTimeout(() => {
-            titdisc.style.fontSize = "25px"
-            disc.style.padding = "20px 0px"
-        }, "1500")
-
+        
     } else {
-        disc.style.maxWidth = "100px"
-        disc.style.height = "50px"
-        disc.style.display = "flex"
-        disc.style.padding = "0px"
-        pardisc.style.display = "none"
+        disc.style.maxHeight = 0
+        disc.style.opacity = 0
+        disc.style.padding = 0
+        titdisc.style.padding = "10px"
         titdisc.style.fontSize = "14px"
+        titdisc.style.width = "150px"
+        disc.style.marginBottom = 0
+        titdisc.style.borderBottom = "5px solid white"
+        titdisc.style.borderRadius = "20px"
         teste = true
     }
 }
 function mus1() {
     var mus = document.getElementById("mus")
-    var parmus = document.getElementById("parmus")
     var titmus = document.getElementById("titmus")
+    var audiomus = document.getElementById("audiomus")
 
     if (teste == true) {
-        mus.style.maxWidth = "80vw"
         if (window.matchMedia("(max-width: 600px)").matches) {
-            mus.style.height = "180vh"
-
-        } else if (window.matchMedia("(max-width: 850px)").matches) {
-            mus.style.height = "140vh"
-        } else {
-            mus.style.height = "100vh"
-        }
-        mus.style.display = "block"
-        parmus.style.marginTop = "50px"
-        mus.style.pointerEvents = "none"
-        teste = false
-
-        setTimeout(() => {
-            parmus.style.display = "block"
-            mus.style.pointerEvents = "auto"
-        }, "2000")
-        setTimeout(() => {
-            titmus.style.fontSize = "25px"
-            mus.style.padding = "20px 0px"
-        }, "1500")
-
-    } else {
-        mus.style.maxWidth = "100px"
-        mus.style.height = "50px"
-        mus.style.display = "flex"
-        mus.style.padding = "0px"
-        parmus.style.display = "none"
+        titmus.style.padding = "10px"
         titmus.style.fontSize = "14px"
+        titmus.style.width = "150px"
+        audiomus.style.width = "240px"
+    } else {
+        titmus.style.padding = "20px"
+        titmus.style.fontSize = "20px"
+        titmus.style.width = "280px"
+        audiomus.style.width = "300px"
+    }
+    mus.style.maxHeight = "300vh"
+    mus.style.opacity = 1
+        mus.style.padding = "30px"
+        mus.style.marginBottom = "50px"
+        titmus.style.borderBottom = "0px"
+        titmus.style.borderRadius = "20px 20px 0px 0px"
+        teste = false
+        
+    } else {
+        mus.style.maxHeight = 0
+        mus.style.opacity = 0
+        mus.style.padding = 0
+        titmus.style.padding = "10px"
+        titmus.style.fontSize = "14px"
+        titmus.style.width = "150px"
+        mus.style.marginBottom = 0
+        titmus.style.borderBottom = "5px solid white"
+        titmus.style.borderRadius = "20px"
         teste = true
     }
 }
